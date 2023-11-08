02a8422b8895479f67fabd60ce161f46b1482407
Ana Spinetti <ana-spinetti@hotmail.com>
2023-11-08 00:22:25 -0300

    Criado endpoint de login inicial (mock) e iniciada a conexão com banco de dados

---
+++ middlewares/conectMongoDB.ts
@@ -0,0 +1,25 @@
+import type { NextApiRequest, NextApiResponse, NextApiHandler } from 'next';
+import type {DefaultResponse} from "../types/DefaultResponse";
+import mongoose from 'mongoose';
+
+export const conectMongoDB = (handler: NextApiHandler) => async (req: NextApiRequest, res: NextApiResponse<DefaultResponse>) => {
+    //Validar se o banco está conectado, caso sim, seguir para o endpoint ou próximo middleware
+    if(mongoose.connections[0].readyState){
+        return handler(req, res);
+    }
+
+    //Conectar ao banco de dados
+    const { DB_CONECTION_STRING } = process.env;
+    if(!DB_CONECTION_STRING){
+        return res.status(500).json({error: "ENV de configuração do banco de dados não informada"})
+    }
+
+    mongoose.connection.on('connected', () => console.log("Banco de dados conectado"));
+    mongoose.connection.on('error', error => console.log("Banco de dados não conectado devido a erro: " + error));
+
+    await mongoose.connect(DB_CONECTION_STRING);
+
+    //Após conectar no banco, seguir para o endpoint
+    return handler(req, res);
+}
+
---
+++ types/DefaultResponse.ts
@@ -0,0 +1,4 @@
+export type DefaultResponse = {
+    msg?: string,
+    error?: string
+}
